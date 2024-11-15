c0c5a7163df4e943a93e54c522e0280862fe7365
Yueyun-555 <s962053@gmail.com>
2024-11-15 14:12:44 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,12 +8,11 @@
 
 
 const urls = [
-	'https://forms.fillout.com/t/5gmjnErHBgus',
+	'https://forms.fillout.com/t/vcdZGss5sYus',
 	'https://forms.fillout.com/t/jeEhaXsoe6us',
 	'https://forms.fillout.com/t/jeEhaXsoe6us',
 	'https://forms.fillout.com/t/1KNz2pQjZYus',
-	'https://forms.fillout.com/t/1KNz2pQjZYus',
-	'https://forms.fillout.com/t/ai1szv2amEus'
+	'https://forms.fillout.com/t/1KNz2pQjZYus'
 	
 
 ];
